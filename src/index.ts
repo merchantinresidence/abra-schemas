21299c4d73d01229b2083c69e7372addda41d1eb
Ryan Macdonald <hi@ryanmacdonald.ca>
2023-05-18 10:17:47 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import DawnSchema from './themes/dawn.json';
+import ExpanseSchema from './themes/expanse.json';
+import FetchSchema from './themes/fetch.json';
+import ImpulseSchema from './themes/impulse.json';
+import MotionSchema from './themes/motion.json';
+import StreamlineSchema from './themes/streamline.json';
+
+export {
+  DawnSchema,
+  ExpanseSchema,
+  FetchSchema,
+  ImpulseSchema,
+  MotionSchema,
+  StreamlineSchema,
+};
+
+export function getSchemaByThemeStoreId(id: number | string) {
+  switch (id.toString()) {
+    case '847': {
+      return MotionSchema;
+    }
+    case '857': {
+      return ImpulseSchema;
+    }
+    case '872': {
+      return StreamlineSchema;
+    }
+    case '887': {
+      return DawnSchema;
+    }
+    case '902': {
+      return ExpanseSchema;
+    }
+    case '1949': {
+      return FetchSchema;
+    }
+    default: {
+      throw new Error('Theme not supported');
+    }
+  }
+}
